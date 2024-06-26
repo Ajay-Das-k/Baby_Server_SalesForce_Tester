2b7538adb96db1076f7373f24164c0a4cc478e7e
Ajay-Das-k <ajtechhub01@gmail.com>
2024-06-26 11:08:24 +0530

    new Babyy Server For Testing innitilized

---
+++ routes/userRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const userRoute = express();
+const path = require("path");
+const userController = require("../controller/userController");
+
+userRoute.use(express.json());
+userRoute.use(express.urlencoded({ extended: true }));
+
+userRoute.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../view", "index.html"));
+});
+
+module.exports = userRoute;
